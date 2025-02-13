dcbd16c8338f6a5823fce0e89125bad5af28285f
Sean Pesis <sean.pesis1@gmail.com>
2025-02-13 14:49:13 +0200

    Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,9 +11,9 @@ function Hero() {
         <p>
           🔭 I’m currently working on side projects and learning various technologies.<br />
           🎓 Studying Computer Science (BSc, 3rd year).<br />
-          👀 Looking to collaborate on CI/CD pipeline, Jenkins, Kubernetes, and Python projects.<br />
+          👀 Looking to collaborate on Full Stack development projects, focusing on modern web technologies, Python projects and robust backend solutions.<br />
           🤔 Seeking guidance in open-source contributions and problem-solving challenges.<br />
-          💬 Feel free to ask me about software development, debugging, or game design.
+          💬 Feel free to ask me about Software development, Full Stack Engineer, or Game design.
         </p>
         <a href="#contact" className="cta-button">Get in Touch</a>
       </div>
